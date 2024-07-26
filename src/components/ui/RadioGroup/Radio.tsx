213d4d162f4a09db15494f17236ec2ed14299b9b
Emanuele Vella <manyuvella@gmail.com>
2024-07-27 01:57:54 +0900

    feat: radio

--- src/components/ui/RadioGroup/Radio.tsx
+++ src/components/ui/RadioGroup/Radio.tsx
@@ -1,12 +1,9 @@
-"use client";
-
+import { cva, cx } from "class-variance-authority";
 import { ReactNode } from "react";
 
 import { Radio as AriaRadio, RadioProps } from "react-aria-components";
-import { tv } from "tailwind-variants";
 
-const focusRing = tv({
-  base: "outline outline-blue-600 dark:outline-blue-500 forced-colors:outline-[Highlight] outline-offset-2",
+const focusRing = cva("outline outline-blue-600 dark:outline-blue-500", {
   variants: {
     isFocusVisible: {
       false: "outline-0",
@@ -15,43 +12,57 @@ const focusRing = tv({
   },
 });
 
-const styles = tv({
-  extend: focusRing,
-  base: "w-5 h-5 rounded-full border-2 bg-white dark:bg-zinc-900 transition-all",
-  variants: {
-    isSelected: {
-      false:
-        "border-gray-400 dark:border-zinc-400 group-pressed:border-gray-500 dark:group-pressed:border-zinc-300",
-      true: "border-[7px] border-gray-700 dark:border-slate-300 forced-colors:!border-[Highlight] group-pressed:border-gray-800 dark:group-pressed:border-slate-200",
+const styles = cva(
+  "w-6 h-6 rounded-full border-2 bg-white dark:bg-zinc-900 transition-all relative",
+  {
+    variants: {
+      isSelected: {
+        false: "border-gray-400 group-pressed:border-gray-500",
+        true: "border-[3px] border-blue-500 group-pressed:border-yellow-800",
+      },
+      isInvalid: {
+        true: "border-red-700 group-pressed:border-red-800",
+      },
+      isDisabled: {
+        true: "border-gray-200",
+      },
     },
-    isInvalid: {
-      true: "border-red-700 dark:border-red-600 group-pressed:border-red-800 dark:group-pressed:border-red-700 forced-colors:!border-[Mark]",
+  }
+);
+
+const selectedCircle = cva(
+  "absolute inset-0 flex items-center justify-center",
+  {
+    variants: {
+      isSelected: {
+        true: "after:w-3 after:h-3 after:rounded-full after:bg-blue-500",
+      },
     },
-    isDisabled: {
-      true: "border-gray-200 dark:border-zinc-700 forced-colors:!border-[GrayText]",
-    },
-  },
-});
+  }
+);
 
 const Radio = (
   props: { children: ReactNode } & Omit<RadioProps, "children">
 ) => {
-  console.log("rrrr", props);
   return (
     <AriaRadio
       {...props}
-      className="flex gap-2 items-center group text-gray-800 disabled:text-gray-300 dark:text-zinc-200 dark:disabled:text-zinc-600 forced-colors:disabled:text-[GrayText] text-sm transition"
+      className="flex gap-2 items-center group text-gray-800 disabled:text-gray-300 text-sm transition"
     >
       {({ isSelected, isInvalid, isDisabled, isFocusVisible }) => (
         <>
           <div
-            className={styles({
-              isSelected,
-              isInvalid,
-              isDisabled,
-              isFocusVisible,
-            })}
-          />
+            className={cx(
+              styles({
+                isSelected,
+                isInvalid,
+                isDisabled,
+              }),
+              focusRing({ isFocusVisible })
+            )}
+          >
+            <div className={selectedCircle({ isSelected })} />
+          </div>
           {props.children}
         </>
       )}
